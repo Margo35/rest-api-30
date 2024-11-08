b40abcd33c75aa51594203141dc837e5b8262024
Puldas Margo <puldas_margarita@mail.ru>
2024-11-08 21:58:29 +0600

    added Spec.java edited ReqresApiTests.java

--- src/test/java/tests/ReqresApiTests.java
+++ src/test/java/tests/ReqresApiTests.java
@@ -10,8 +10,9 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.Spec.*;
 
-public class ReqresApiTests {
+public class ReqresApiTests extends TestBase {
 
     @Test
     void successfulLoginTest() {
@@ -20,25 +21,18 @@ public class ReqresApiTests {
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("cityslicka");
 
-        LoginResponseModel response = step("Отправляем POST запрос на логин", ()->
-         given()
-                .filter(withCustomTemplates())
-                .body(authData)
-                .contentType(JSON)
-                .log().uri()
-                .log().body()
-                .log().headers()
-        .when()
-                .post("https://reqres.in/api/login")
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponseModel.class));
+        LoginResponseModel response = step("Отправляем POST запрос на логин", () ->
+                given(requestSpec)
+                        .body(authData)
+                .when()
+                        .post("/login")
+                .then()
+                        .spec(responseSpec200)
+                        .extract().as(LoginResponseModel.class));
 
-        step("Проверяем полученный токен при успешном логине", ()->
-        assertEquals("QpwL5tke4Pnpja7X4",response.getToken()));
-       
+        step("Проверяем полученный токен при успешном логине", () ->
+                assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+
     }
 
     @Test
@@ -48,82 +42,66 @@ public class ReqresApiTests {
         userData.setName("morpheus");
         userData.setJob("leader");
 
-      UserDataResponseModel response = step("Отправляем POST запрос добавление юзера", ()-> given()
-                .filter(withCustomTemplates())
-                .body(userData)
-                .contentType(JSON)
-                .log().uri()
-                .log().body()
-                .log().headers()
-        .when()
-                .post("https://reqres.in/api/users")
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(201)
-                .extract().as(UserDataResponseModel.class));
+        UserDataResponseModel response = step("Отправляем POST запрос добавление юзера", () ->
+                given(requestSpec)
+                        .body(userData)
+                .when()
+                        .post("/users")
+                .then()
+                        .spec(responseSpec201)
+                        .extract().as(UserDataResponseModel.class));
 
-        step("Проверяем имя и должность созданного юзера", ()->{
-        assertEquals("morpheus",response.getName());
-        assertEquals("leader",response.getJob());});
+        step("Проверяем имя и должность созданного юзера", () -> {
+            assertEquals("morpheus", response.getName());
+            assertEquals("leader", response.getJob());
+        });
     }
 
     @Test
     void getUsersTest() {
 
-        UserDataMainResponseModel response = step("Отправляем GET запрос на получение юзеров со 2-ой страницы", ()->
-                given()
-                .filter(withCustomTemplates())
-                .log().uri()
-        .when()
-                .get("https://reqres.in/api/users?page=2")
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(UserDataMainResponseModel.class));
+        UserDataMainResponseModel response = step("Отправляем GET запрос на получение юзеров со 2-ой страницы", () ->
+                given(requestSpec)
+                .when()
+                        .get("/users?page=2")
+                .then()
+                        .spec(responseSpec200)
+                        .extract().as(UserDataMainResponseModel.class));
 
-        step("Проверяем имя полученного юзера", ()->
-        assertEquals("Michael",response.getData().get(0).getFirst_name()));
+        step("Проверяем имя полученного юзера", () ->
+                assertEquals("Michael", response.getData().get(0).getFirst_name()));
 
     }
 
     @Test
     void getSingleUserTest() {
 
-       UserSingleResponseModel response = step("Отправляем GET запрос на получение 2-го юзера", ()->
-                 given()
-                .filter(withCustomTemplates())
-                .log().uri()
-        .when()
-                .get("https://reqres.in/api/users/2")
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(UserSingleResponseModel.class));
+        UserSingleResponseModel response = step("Отправляем GET запрос на получение 2-го юзера", () ->
+                given(requestSpec)
+                .when()
+                        .get("/users/2")
+                .then()
+                        .spec(responseSpec200)
+                        .extract().as(UserSingleResponseModel.class));
 
-        step("Проверяем данные полученного юзера", ()->{
-        assertEquals(2,response.getData().getId());
-        assertEquals("janet.weaver@reqres.in",response.getData().getEmail());
-        assertEquals("Janet",response.getData().getFirst_name());
-        assertEquals("Weaver",response.getData().getLast_name());});
+        step("Проверяем данные полученного юзера", () -> {
+            assertEquals(2, response.getData().getId());
+            assertEquals("janet.weaver@reqres.in", response.getData().getEmail());
+            assertEquals("Janet", response.getData().getFirst_name());
+            assertEquals("Weaver", response.getData().getLast_name());
+        });
 
     }
 
     @Test
     void checkUserNotFoundTest() {
 
-        step("Отправляем GET запрос на получение 13-го юзера", ()->
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-        .when()
-                .get("https://reqres.in/api/users/13")
-        .then()
-                .log().status()
-                .log().body()
-                .statusCode(404));
+        step("Отправляем GET запрос на получение несуществующего 13-го юзера", () ->
+                given(requestSpec)
+                .when()
+                        .get("/users/13")
+                .then()
+                        .spec(responseSpec404));
     }
 
 }
